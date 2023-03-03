84b0303ed33fa84a542decc350a0a28f4ba0c0b6
thaolaptrinh <thaolaptrinh@gmail.com>
2023-03-03 14:03:18 +0700

    config app

---
+++ configs/Routes.php
@@ -0,0 +1,36 @@
+<?php
+
+$routes[''] = 'teacher';
+$routes['default_controller'] = 'teacher';
+$routes['dashboard'] = 'teacher';
+
+
+//Auth  
+// $routes['login'] = 'auth/login';
+// $routes['register'] = 'auth/register';
+// $routes['reset-pasword'] = 'auth/resetpass'; 
+$routes['auth/logout'] = 'teacher/logout';
+$routes['auth/reset-password'] = 'auth/resetpass';
+
+// Quản lý
+$routes['danh-muc/khoa-hoc'] = 'teacher/khoa_hoc';
+$routes['danh-muc/hoc-ky'] = 'teacher/hoc_ky';
+$routes['danh-muc/mon-hoc'] = 'teacher/mon_hoc';
+$routes['danh-muc/loai-lop'] = 'teacher/loai_lop';
+$routes['danh-muc/loai-diem'] = 'teacher/loai_diem';
+$routes['bang-diem'] = 'teacher/bang_diem';
+
+// Lớp học
+$routes['lich-day'] = 'teacher/lich_day';
+
+// Tiện ích
+$routes['tien-ich/thanh-ngu'] = 'teacher/thanh_ngu';
+$routes['tien-ich/lien-ket'] = 'teacher/links';
+$routes['profile'] = 'teacher/profile';
+
+
+//Admin 
+$routes['admin'] = 'admin/home';
+$routes['admin/quan-ly/site'] = 'admin/home/site';
+$routes['admin/quan-ly/co-so'] = 'admin/home/co_so';
+$routes['admin/giao-vien'] = 'admin/home/teachers';
